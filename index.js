910b6641ece2d3f0155f4d805fb3c091db0ff636
Paul Buskovskij <paul.buskovskij@konzepthaus-ws.de>
2024-09-06 00:06:00 +0200

    reorder routes

--- index.js
+++ index.js
@@ -11,17 +11,48 @@ app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
 const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
+const PETS_OBJECT_TYPE = process.env.PETS_OBJECT_TYPE;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
-app.get("/update-cobj", async (req, res) => {
-  res.render("updates");
+app.get("/", async (req, res) => {
+  const route = `https://api.hubspot.com/crm/v3/objects/${PETS_OBJECT_TYPE}?properties=name,age,species`;
+  const headers = {
+      Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+      'Content-Type': 'application/json'
+  }
+  try {
+      const response = await axios.get(route, { headers });
+      const data = response.data.results;
+      data.forEach(item => console.log(item.properties));
+      res.render('homepage', { title: 'Pets | HubSpot APIs', data });
+  } catch (error) {
+      console.error(error);
+  }
 });
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 // * Code for Route 2 goes here
+app.get("/update-cobj", async (req, res) => {
+  const data = {
+      name: "Goldy",
+      age: 123,
+      species: "Fish",
+  };
+
+  try {
+    await axios.post("http://localhost:3000/update-cobj", data);
+    res.render("updates");
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
+
+// * Code for Route 3 goes here
 app.post("/update-cobj", async (req, res) => {
   const data = {
     properties: {
@@ -31,7 +62,7 @@ app.post("/update-cobj", async (req, res) => {
     },
   };
 
-  const route = "https://api.hubapi.com/crm/v3/objects/2-133147488";
+  const route = `https://api.hubapi.com/crm/v3/objects/${PETS_OBJECT_TYPE}`;
   const headers = {
     Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
     "Content-Type": "application/json",
@@ -44,10 +75,6 @@ app.post("/update-cobj", async (req, res) => {
     console.error(err);
   }
 });
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
 
 /**
 * * This is sample code to give you a reference for how you should structure your calls.
